42fbc17589861501f64ddca0bdf4bc18d722b29f
James <James.Steel@me.com>
2016-10-03 13:30:42 +0100

    added oyster card balance

---
+++ lib/oyster_card.rb
@@ -0,0 +1,10 @@
+class OysterCard
+
+attr_accessor :balance
+
+ def initialize
+   @balance = 0
+ end
+
+
+end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,3 +1,11 @@
+require 'oyster_card'
+
 describe OysterCard do
-	
+
+   describe '#initalize' do
+		it 'creates a card with a balance of 0' do
+			expect(subject.balance).to eq 0
+		end
+
+   end
 end
